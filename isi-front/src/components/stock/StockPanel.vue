cbc06c73ca77ab1ed61dfad23f58a7370ef4705e
O. Saifutdinov <zeusfly@mail.ru>
2019-06-25 12:29:43 +0300

    Created stock panel page

--- isi-front/src/components/stock/StockPanel.vue
+++ isi-front/src/components/stock/StockPanel.vue
@@ -1,11 +1,35 @@
 <template>
     <v-flex align-center>
-        Склад
+        <span class="title blue-grey--text">Каталог продукции</span>
+        <v-data-table
+            :headers="headers"
+            :items="insoles"
+            hide-actions
+            class="elevation-1"
+        >
+            <template v-slot:items="props">
+                <td>{{ props.item.id }}</td>
+                <td>{{ props.item.type }}</td>
+                <td>{{ props.item.size }}</td>
+                <td>{{ props.item.material }}</td>
+            </template>
+            <template v-slot:no-data>
+                <span class="red--text">Нет продукции</span>
+            </template>
+        </v-data-table>
     </v-flex>
 </template>
 <script>
     export default {
         name: 'StockPanel',
+        data: () => ({
+            headers: [
+                {text: '#', value: 'id'},
+                {text: 'Тип', value: 'type'},
+                {text: 'Размер', value: 'size'},
+                {text: 'Материал', value: 'material'}
+            ]
+        }),
         computed: {
             insoles () {
                 return this.$store.state.insoles
