cd0bc89a766bbe2bc32a1d9bdfd3e85ddd518fe1
O. Saifutdinov <zeusfly@mail.ru>
2019-06-28 18:45:12 +0300

    Fixed WorkDaysTable component table layout

--- isi-front/src/components/daily/WorkDaysTable.vue
+++ isi-front/src/components/daily/WorkDaysTable.vue
@@ -6,12 +6,16 @@
                 hide-actions
             >
                 <template v-slot:items="props">
-                    <td>{{ props.item.user.full_name }}</td>
-                    <td>{{ props.item.working_hours }}</td>
-                    <td>{{ props.item.time_start }}</td>
-                    <td>{{ props.item.time_finish}}</td>
-                    <td>{{ props.item.dinner_start }} - {{ props.item.dinner_finish }}</td>
-                    <td>Действия</td>
+                    <tr :class="{'working': props.item.working && isToday}"
+                        style="background: #EF9A9A"
+                    >
+                        <td>{{ props.item.user.full_name }}</td>
+                        <td>{{ props.item.working_hours }}</td>
+                        <td>{{ hideSeconds(props.item.time_start) }}</td>
+                        <td>{{ props.item.time_finish}}</td>
+                        <td>{{ props.item.dinner_start }} - {{ props.item.dinner_finish }}</td>
+                        <td>Действия</td>
+                    </tr>
                 </template>
                 <template v-slot:no-data>
                     <span class="red--text">Нет записей</span>
@@ -68,9 +72,18 @@
             }
         },
         methods: {
+            hideSeconds (time) {
+                let params = time.split(':')
+                return `${params[0]}:${params[1]}`
+            },
             startDay () {
                 this.$store.dispatch('startUserDay')
             }
         }
     }
 </script>
+<style>
+    .working {
+        border-bottom: 2pt solid #fafafa!important;
+    }
+</style>
