e5ad352b8f03a02334fbeedcd60331ec383eebcc
O. Saifutdinov <zeusfly@mail.ru>
2019-07-01 18:20:23 +0300

    Added Expense model with migration & added balance calculating methods on Island model

--- app/Island.php
+++ app/Island.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Island extends Model
@@ -12,4 +13,47 @@ class Island extends Model
     {
         return $this->hasMany(User::class);
     }
+
+    public function startDays()
+    {
+        return $this->hasMany(StartDay::class);
+    }
+
+    public function deals()
+    {
+        return $this->hasMany(Deal::class);
+    }
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function dateBalance(string $date)
+    {
+        return $this->deals()->whereDate('created_at', $date)->get()
+            ->reduce(function ($acc, $deal) {
+                return $acc + $deal->income - $deal->expense;
+            }, $this->startBalance($date));
+    }
+
+    public function startBalance(string $date)
+    {
+        return $this->startDays()->whereDate('created_at', $date)->first()->amount ?? 0;
+    }
+
+    public function dateExpenses(string $date)
+    {
+        return $this->expenses()->whereDate('created_at', $date)->get()
+            ->reduce(function ($acc, $item) {
+                return $acc + $item->amount;
+            }, 0);
+    }
+
+    public function makeStartDate()
+    {
+        $yesterday = Carbon::yesterday()->toDateString();
+        return $this->startDays()->create(['amount' => $this->dateBalance($yesterday) - $this->dateExpenses($yesterday)]);
+    }
+
 }
