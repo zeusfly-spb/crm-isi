a5add219ca677bf853e9119a2db9e97fb4d583a7
O. Saifutdinov <zeusfly@mail.ru>
2019-07-02 11:15:58 +0300

    Added artisan command for calculate island balances

---
+++ app/Console/Commands/CalculateBalances.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Island;
+
+class CalculateBalances extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'calculate:balances';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Calculate Islands balances & store StartDay amount';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $islands = Island::all();
+        foreach ($islands as $island) {
+            $this->info($island->name . ' = ' .$island->makeStartDate()->amount . PHP_EOL);
+        }
+    }
+}
