625e94d858c780f2517dcf6df1fe2dc0bc747292
O. Saifutdinov <zeusfly@mail.ru>
2019-09-16 18:14:15 +0300

    Added user photo on IslandControl component table

--- isi-front/src/components/admin/IslandUsersColumn.vue
+++ isi-front/src/components/admin/IslandUsersColumn.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        <div v-for="(user, index) in users" :key="index">{{ user.full_name }}</div>
+        <div v-for="(user, index) in users" :key="index">
+            <v-avatar
+                size="30px"
+                :key="user.id"
+                style="margin-bottom: .2em"
+            >
+                <img :src="`${basePath}${user.avatar ? user.avatar : '/img/default.jpg'}`"
+                     alt="Фото"
+                     :title="user.full_name"
+                />
+            </v-avatar>
+
+            {{ user.full_name }}
+        </div>
         <div v-if="!users.length" class="grey--text">-</div>
     </div>
 </template>
 <script>
     export default {
         name: 'IslandUsersColumn',
-        props: ['users']
+        props: ['users'],
+        computed: {
+            basePath () {
+                return this.$store.state.basePath
+            }
+        }
     }
 </script>
