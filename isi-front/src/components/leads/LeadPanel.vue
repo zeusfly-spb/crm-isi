36f0e8d8d571dda0c1c0c0e1eeec586e52beebc9
O. Saifutdinov <zeusfly@mail.ru>
2019-09-17 19:40:19 +0300

    Creating delete lead feature

--- isi-front/src/components/leads/LeadPanel.vue
+++ isi-front/src/components/leads/LeadPanel.vue
@@ -8,6 +8,16 @@
             class="elevation-1"
         >
             <template v-slot:items="props">
+                <td>
+                    <v-icon
+                        class="red--text delete"
+                        :title="`Удалить заявку на номер ${props.item.phone}`"
+                        @click="confirmToDelete(props.item)"
+                        v-if="isSuperadmin"
+                    >
+                        clear
+                    </v-icon>
+                </td>
                 <td>{{ props.index + 1 }}</td>
                 <td>{{ props.item.name }}</td>
                 <td>
@@ -22,6 +32,32 @@
                 <span class="red--text">Нет заявок</span>
             </template>
         </v-data-table>
+        <v-dialog v-model="confirm"
+                  max-width="400"
+        >
+            <v-card>
+                <v-card-title class="subheading">
+                    {{ confirmText }}
+                </v-card-title>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        flat="flat"
+                        @click="confirm = false"
+                    >
+                        Отмена
+                    </v-btn>
+                    <v-btn
+                        color="red darken-1"
+                        flat="flat"
+                        @click=""
+                    >
+                        Удалить
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+
+        </v-dialog>
     </v-flex>
 </template>
 <script>
@@ -29,8 +65,12 @@
     export default {
         name: 'LeadsPanel',
         data: () => ({
+            confirmText: '',
+            confirm: false,
+            leadToDelete: null,
             headers: [
-                {text: '№', value: 'id'},
+                {text: '', value: null},
+                {text: '#', value: 'id'},
                 {text: 'Имя', value: 'name'},
                 {text: 'Телефон', value: 'phone'},
                 {text: 'Сайт', value: 'site'},
@@ -39,8 +79,18 @@
             ]
         }),
         computed: {
+            isSuperadmin () {
+                return this.$store.getters.isSuperadmin
+            },
             leads () {
                 return this.$store.state.loader.leads
+            }
+        },
+        methods: {
+            confirmToDelete (lead) {
+                this.leadToDelete = lead
+                this.confirmText = `Удалить заявку с номера ${lead.phone}?`
+                this.confirm = true
             }
         },
         components: {
@@ -48,3 +98,12 @@
         }
     }
 </script>
+<style scoped>
+    .delete {
+        cursor: pointer;
+        opacity: .6;
+    }
+    .delete:hover {
+        opacity: 1;
+    }
+</style>
