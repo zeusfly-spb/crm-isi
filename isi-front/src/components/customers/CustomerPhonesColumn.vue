a57a685b828f1cc0738820049f4e370eee73ea75
O. Saifutdinov <zeusfly@mail.ru>
2019-09-23 18:33:05 +0300

    Applied phones mask on customers table

--- isi-front/src/components/customers/CustomerPhonesColumn.vue
+++ isi-front/src/components/customers/CustomerPhonesColumn.vue
@@ -11,8 +11,12 @@
         props: ['phones'],
         filters: {
             phone: function (val) {
+                String.prototype.replaceAt = function(index, replacement) {
+                    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+                }
                 return '+7 ' + val.replace(/[^0-9]/g, '')
                     .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')
+                    .replaceAt(6, '*').replaceAt(7, '*').replaceAt(8, '*')
             }
         }
     }
