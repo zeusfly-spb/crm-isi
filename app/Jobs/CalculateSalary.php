fca324c21fb63a114207003fc82f10cf815bb82b
O. Saifutdinov <zeusfly@mail.ru>
2019-12-27 12:56:07 +0300

    Changed cache type & created CalculateSalary job class

---
+++ app/Jobs/CalculateSalary.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Listeners\DealPerformedListener;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+
+class CalculateSalary implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $model;
+    protected $island_id;
+    protected $date;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Model $model)
+    {
+        $this->island_id = $model->island_id;
+        $this->date = explode(' ', $model->created_at)[0];
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $dateArray = explode('-', $this->date);
+        array_pop($dateArray);
+        $monthStr = implode('-', $dateArray);
+        $cache_name = 'salary_' . $this->island_id . '_' . $monthStr;
+        Cache::forget($cache_name);
+        Cache::put($cache_name, DealPerformedListener::retrieveMonthData($this->date, $this->island_id));
+        $cache_name = 'salary_0_' . $monthStr;
+        Cache::forget($cache_name);
+        Cache::put($cache_name, DealPerformedListener::retrieveMonthData($this->date, 0));
+    }
+}
