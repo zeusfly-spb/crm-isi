d61b44be3668f8cb0757428f0012461215d308af
O. Saifutdinov <zeusfly@mail.ru>
2020-02-28 11:09:11 +0300

    Fixed migration to add status field into appointments table

--- database/migrations/2020_02_27_170212_add_status_field_into_appointments_table.php
+++ database/migrations/2020_02_27_170212_add_status_field_into_appointments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusDescriptionFieldIntoAppointmentsTable extends Migration
+class AddStatusFieldIntoAppointmentsTable extends Migration
 {
     /**
      * Run the migrations.
