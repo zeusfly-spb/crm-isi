6c740c39c02320df3c3f8a67d2790921a7e26c67
O. Saifutdinov <zeusfly@mail.ru>
2020-03-18 15:06:55 +0300

    Created event control chip components styles

--- isi-front/src/components/appointments/EventControlChip.vue
+++ isi-front/src/components/appointments/EventControlChip.vue
@@ -1,5 +1,35 @@
 <template>
     <v-chip>
+        <v-speed-dial
+            v-model="fab"
+            open-on-hover
+            transition="slide-y-reverse-transition"
+        >
+            <template v-slot:activator>
+                <v-icon
+                    style="margin-top: 3px"
+                    size="20"
+                    :color="fabStyle.color"
+                    class="clickable"
+                    title="Сменить статус записи"
+                >
+                    {{ fabStyle.icon }}
+                </v-icon>
+            </template>
+            <v-btn
+                v-for="(item, index) in statsButtons"
+                :key="index"
+                fab
+                dark
+                small
+                :color="item.color"
+                :title="item.title"
+            >
+                <v-icon>
+                    {{ item.icon }}
+                </v-icon>
+            </v-btn>
+        </v-speed-dial>
         <v-icon
             size="20"
             color="green"
@@ -25,7 +55,24 @@
     export default {
         name: 'EventControlChip',
         props: ['event'],
+        data: () => ({
+            fab: false,
+            statsRaw: [
+                {status: 'active', color: 'blue', icon: 'event', title: 'Сменить статус на "Активно"'},
+                {status: 'completed', color: 'green', icon: 'event_available', title: 'Сменить статус на "Выполнено"'},
+                {status: 'cancelled', color: 'red', icon: 'event_busy', title: 'Сменить статус на "Отменено"'},
+            ]
+        }),
         computed: {
+            statsButtons () {
+                return this.statsRaw.filter(item => item.status !== this.event.status)
+            },
+            fabStyle () {
+                return {
+                    color: {active: 'blue', completed: 'green', cancelled: 'red'}[this.event.status],
+                    icon: {active: 'event', completed: 'event_available', cancelled: 'event_busy'}[this.event.status]
+                }
+            },
             isSuperadmin () {
                 return this.$store.getters.isSuperadmin
             }
