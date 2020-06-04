0737ba591d75e735f5f4788e401603434ebc7aa0
O. Saifutdinov <zeusfly@mail.ru>
2020-06-04 13:52:56 +0300

    Created save sms report feature when sending sms from backend

--- app/helpers.php
+++ app/helpers.php
@@ -3,19 +3,36 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 use GuzzleHttp\Client;
+use App\Jobs\CreateSmsReport;
+
+/**
+ * @param array $data
+ * ['extension', 'phone', 'text', 'user_id', 'island_id']
+ * @return int
+ * @throws \GuzzleHttp\Exception\GuzzleException
+ */
 
 function sendSms (array $data) {
     $client = new Client();
     $res = $client->request('POST', 'https://crmkin.ru/tel/api/vpbx/sms/send', [
         'form_params' => [
             'base_type' => 'isi',
-            'user_id' => $data['user_id'],
+            'user_id' => $data['user_id'] ?? 0,
             'extension' => $data['extension'],
             'phone' => $data['phone'],
             'text' => $data['text']
         ]
     ]);
-    return $res->getStatusCode();
+    $status = $res->getStatusCode();
+    if ($status == 200) {
+        CreateSmsReport::dispatch([
+            'number' => $data['phone'],
+            'text' => $data['text'],
+            'user_id' => $data['user_id'] ?? 0,
+            'island_id' => $data['island_id'] ?? 0
+        ]);
+    }
+    return $status;
 }
 
 function clearPostponesCache () {
