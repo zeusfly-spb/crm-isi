4dc6b19d8b1a7b90898a099c79f17a0f96e5efe0
O. Saifutdinov <zeusfly@mail.ru>
2020-06-11 19:43:25 +0300

    Created notification table functionality

--- isi-front/src/components/appointments/NotificationTable.vue
+++ isi-front/src/components/appointments/NotificationTable.vue
@@ -7,6 +7,48 @@
             :items="smsReports"
             hide-actions
         >
+            <template v-slot:items="props">
+                <tr>
+                    <td
+                        align="left"
+                        class="index-column"
+                    >
+                        {{ props.index + 1}}
+                    </td>
+                    <td
+                        align="left"
+                        class="user-column"
+                    >
+                        <user-avatar
+                            v-if="props.item.user"
+                            :user="props.item.user"
+                        />
+                        <v-avatar
+                                v-if="!props.item.user"
+                                size="36px"
+                                title="Служба информирования"
+                        >
+                            <img
+                                :src="`${basePath}/img/logo.png`"
+                            />
+                        </v-avatar>
+                    </td>
+                    <td
+                        class="phone-column"
+                    >
+                        {{ props.item.number.slice(2) | phone }}
+                    </td>
+                    <td>
+                        {{ props.item.text }}
+                    </td>
+                    <td
+                        align="right"
+                        class="time-column"
+                    >
+                        {{ props.item.created_at | moment('H:mm') }}
+                    </td>
+                </tr>
+            </template>
             <template v-slot:no-data>
                 <span class="red--text">Нет оповещений от {{ eventsDate | moment('D MMMM YYYY г.') }}</span>
             </template>
@@ -15,6 +57,7 @@
 </template>
 
 <script>
+    import UserAvatar from '../main/UserAvatar'
     export default {
         name: 'NotificationTable',
         data: () => ({
@@ -23,10 +66,13 @@
                 {text: 'Отправитель', value: 'user_id'},
                 {text: 'Номер', value: 'number'},
                 {text: 'Текст', value: 'text'},
-                {text: 'Время', value: 'created_at'}
+                {text: 'Время', value: 'created_at', align: 'right'}
             ]
         }),
         computed: {
+            basePath () {
+                return this.$store.state.basePath
+            },
             eventsDate () {
                 return this.$store.getters.eventsDate
             },
@@ -34,7 +80,12 @@
                 return this.$store.state.informer.smsReportsLoading
             },
             smsReports () {
-                return this.$store.state.informer.smsReports
+                const attachUsers = reports => reports.reverse()
+                    .map(item => ({
+                        ... item,
+                        user: this.$store.state.users.find(user => +user.id === +item.user_id) || null
+                    }))
+                return attachUsers(this.$store.state.informer.smsReports)
             }
         },
         methods: {
@@ -52,10 +103,24 @@
                 }
                 this.setSmsReports()
             }
+        },
+        components: {
+            UserAvatar
         }
     }
 </script>
 
 <style scoped>
-
+    .index-column {
+        width: 2em;
+    }
+    .user-column {
+        width: 2em;
+    }
+    .phone-column {
+        width: 12em;
+    }
+    .time-column {
+        width: 3em;
+    }
 </style>
