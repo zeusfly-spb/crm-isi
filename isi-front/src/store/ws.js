ace4a132d8bef6dda0222ff055980ee7399a6ec0
O. Saifutdinov <zeusfly@mail.ru>
2020-06-24 19:28:34 +0300

    Created lead delete by ws data feature

--- isi-front/src/store/ws.js
+++ isi-front/src/store/ws.js
@@ -13,11 +13,16 @@ export default {
     actions: {
         handleSqlEvent ({commit}, event) {
             const insertLead = lead => commit('ADD_LEAD', lead)
+            const deleteLead = lead => commit('DELETE_LEAD', lead)
 
             const handleLead = event => {
                 if (event.type === 'INSERT' && event.affectedRows[0].after) {
                     insertLead(event.affectedRows[0].after)
                 }
+                if (event.type === 'DELETE' && event.affectedRows[0].before) {
+                    deleteLead(event.affectedRows[0].before)
+                }
+
             }
 
             const parseEvent = event => {
