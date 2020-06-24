54a22d00885fc67c89c4d707baa4de50c77c3cc6
O. Saifutdinov <zeusfly@mail.ru>
2020-06-24 20:42:36 +0300

    Created update lead feature

--- isi-front/src/store/ws.js
+++ isi-front/src/store/ws.js
@@ -14,10 +14,16 @@ export default {
         handleSqlEvent ({commit}, event) {
             const insertLead = lead => commit('ADD_LEAD', lead)
             const deleteLead = lead => commit('DELETE_LEAD', lead)
+            const updateLead = lead => commit('UPDATE_LEAD', lead)
 
             const handleLead = event => {
                 if (event.type === 'INSERT' && event.affectedRows[0].after) {
                     insertLead(event.affectedRows[0].after)
+                }
+                if (event.type === 'UPDATE' && event.affectedRows[0].after) {
+                    console.log('Updating')
+                    console.dir(event.affectedRows[0].after)
+                    updateLead(event.affectedRows[0].after)
                 }
                 if (event.type === 'DELETE' && event.affectedRows[0].before) {
                     deleteLead(event.affectedRows[0].before)
