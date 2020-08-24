3209c7402e5e2002a5be30b27bac99640702e3f7
O. Saifutdinov <zeusfly@mail.ru>
2020-08-24 16:54:31 +0300

    Fixed constraints to process deal frames when current page is not daily

--- isi-front/src/store/ws.js
+++ isi-front/src/store/ws.js
@@ -112,7 +112,7 @@ export default {
                     case 'deal':
                         getters.accountingDate !== dealDate(obj.model) ? result = false : null
                         getters.workingIslandId !== 0 && obj.model.island_id !== getters.workingIslandId ? result = false : null
-                        getters.currentPage !== 'daily' ? result = false : null
+                        // getters.currentPage !== 'daily' ? result = false : null
                         break
                     case 'appointment':
                         month(getters.eventsDate) !== month(obj.model.date) ? result = false : null
