b22660d75c09ac8ff473a0c931de6687be3f914c
O. Saifutdinov <zeusfly@mail.ru>
2020-09-01 16:05:13 +0300

    Creating performer module salary caching features belonged to conditions

--- microservices/observer.js
+++ microservices/observer.js
@@ -1,39 +1,13 @@
 const mysql = require('mysql')
 const mySqlEvents = require('@rodrigogs/mysql-events')
 const CONFIG = require('./config')
-const salaryController = require('./controllers/SalaryController')
+const performer = require('./performer')
 
 const connection = mysql.createConnection({
     host: CONFIG.db_host,
     user: CONFIG.db_user,
     password: CONFIG.db_password
 })
-
-const { broadcast } = require('./transmitter')
-const clearDate = datetime => datetime.includes('T') ? datetime.split('T')[0] : datetime.split(' ')[0]
-
-const handleEvent = event => {
-    if (event.schema !== CONFIG.db_name) {
-        return
-    }
-    if (event.table === 'leads' && event.type === 'INSERT') {
-        let frame = {
-            type: 'add_lead',
-            model: event.affectedRows[0].after
-        }
-        broadcast(JSON.stringify(frame))
-    }
-    if (['deals', 'work_days', 'appointments', 'prepays', 'prizes', 'sicks', 'vacations'].includes(event.table)) {
-        console.time('Caching salary month data')
-        let row = event.type === 'DELETE' ? event.affectedRows[0].before : event.affectedRows[0].after
-        let targetDate = (['appointments', 'work_days'].includes(event.table) ? row.date : row.created_at).toISOString()
-        Promise.all([
-            salaryController.cacheMonthData({date: clearDate(targetDate), island_id: row.island_id}),
-            salaryController.cacheMonthData({date: clearDate(targetDate)})
-        ])
-            .then(() => console.timeEnd('Caching salary month data'))
-    }
-}
 
 const program = async () => {
     const instance = new mySqlEvents(connection, {
@@ -47,7 +21,7 @@ const program = async () => {
         name: 'TEST',
         expression: '*',
         statement: mySqlEvents.STATEMENTS.ALL,
-        onEvent: event => handleEvent(event)
+        onEvent: event => performer.inspect(event)
     })
     instance.on(mySqlEvents.EVENTS.CONNECTION_ERROR, console.error)
     instance.on(mySqlEvents.EVENTS.ZONGJI_ERROR, console.error)
---
+++ microservices/performer.js
@@ -0,0 +1,62 @@
+const chalk = require('chalk')
+const CONFIG = require('./config')
+const { broadcast } = require('./transmitter')
+
+const salaryController = require('./controllers/SalaryController')
+
+const clearDate = datetime => datetime.includes('T') ? datetime.split('T')[0] : datetime.split(' ')[0]
+const targetRow = event => event.type === 'DELETE' ? event.affectedRows[0].before : event.affectedRows[0].after
+const targetDate = event => (['appointments', 'work_days']
+    .includes(event.table) ? targetRow(event).date : targetRow(event).created_at).toISOString()
+
+const cacheSalary = event => {
+    console.time('Cache salary month data')
+    Promise.all([
+        salaryController.cacheMonthData({date: clearDate(targetDate(event)), island_id: targetRow(event).island_id}),
+        salaryController.cacheMonthData({date: clearDate(targetDate(event))})
+    ])
+        .then(() => console.timeEnd('Cache salary month data'))
+}
+
+const performLead = event => event.type === 'INSERT' ?
+    broadcast(JSON.stringify({type: 'add_lead', model: event.affectedRows[0].after})) : null
+
+const performDeal = event => cacheSalary(event)
+
+const performWorkDay = event => event.type === 'UPDATE' && event.affectedColumns.includes('working_hours') ?
+    cacheSalary(event) : null
+
+/*
+if (['deals', 'work_days', 'appointments', 'prepays', 'prizes', 'sicks', 'vacations'].includes(event.table)) {
+    console.time('Caching salary month data')
+    let row = event.type === 'DELETE' ? event.affectedRows[0].before : event.affectedRows[0].after
+    let targetDate = (['appointments', 'work_days'].includes(event.table) ? row.date : row.created_at).toISOString()
+    Promise.all([
+        salaryController.cacheMonthData({date: clearDate(targetDate), island_id: row.island_id}),
+        salaryController.cacheMonthData({date: clearDate(targetDate)})
+    ])
+        .then(() => console.timeEnd('Caching salary month data'))
+}
+ */
+
+const inspect = event => {
+    if (event.schema !== CONFIG.db_name) {
+        return
+    }
+    switch (event.table) {
+        case 'leads':
+            performLead(event)
+            break
+        case 'deals':
+            performDeal(event)
+            break
+        case 'work_days':
+            performWorkDay(event)
+            break
+        default: break
+    }
+}
+
+module.exports = {
+    inspect
+}
