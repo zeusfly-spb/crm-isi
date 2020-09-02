72cf4c286b29e2ac57eaad025515a287a4c687bd
O. Saifutdinov <zeusfly@mail.ru>
2020-09-02 19:28:35 +0300

    Fixed appointment context menu bug

--- isi-front/src/components/appointments/FirstEvent.vue
+++ isi-front/src/components/appointments/FirstEvent.vue
@@ -1,52 +1,65 @@
 <template>
-    <span>
+    <v-menu
+            v-if="!$store.state.appointment.draggedEvent"
+            v-model="menu"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            :nudge-right="40"
+            :nudge-bottom="-10"
+            lazy
+            offset-y
+            full-width
+            min-width="290px"
+    >
+        <template v-slot:activator="data">
+             <span>
         <v-btn
-            flat
-            round
-            outline
-            v-if="!display"
-            :draggable="event.draggable"
-            :title="mainTitle"
-            :style="{
+                flat
+                round
+                outline
+                v-if="!display"
+                :draggable="event.draggable"
+                :title="mainTitle"
+                :style="{
                 'cursor': !event.draggable ? 'pointer' : dragging ? 'grabbing' : 'grab',
                 'min-width': compact ? 0 : '88px',
                 'background-color': `${background}!important`
             }"
-            :ripple="false"
-            :id="`first-${event.id}`"
-            @click="show"
-            @dragstart="dragStart"
-            @dragend="dragEnd"
-            @dragenter="dragEnter"
-            @dragover="dragEnter"
-            @dragleave="dragEnter"
-            @mousedown="dragging = true"
-            @mouseup="dragging = false"
-            @contextmenu.prevent="menu = true"
+                :ripple="false"
+                :id="`first-${event.id}`"
+                @click="show"
+                @dragstart="dragStart"
+                @dragend="dragEnd"
+                @dragenter="dragEnter"
+                @dragover="dragEnter"
+                @dragleave="dragEnter"
+                @mousedown="dragging = true"
+                @mouseup="dragging = false"
+                @contextmenu.prevent="menu = true; data.on"
         >
             <v-icon
-                :color="colors[event.status]"
-                class="ml-1"
-                :title="`Статус: ${ {active: 'Активна', postponed: 'Отложена', moderate: 'На модерации', completed: 'Завершена', cancelled: 'Отказ'}[event.status] }`"
+                    :color="colors[event.status]"
+                    class="ml-1"
+                    :title="`Статус: ${ {active: 'Активна', postponed: 'Отложена', moderate: 'На модерации', completed: 'Завершена', cancelled: 'Отказ'}[event.status] }`"
             >
                 {{ icons[event.status] }}
             </v-icon>
             <span
-                v-if="!compact"
-                class="green--text"
+                    v-if="!compact"
+                    class="green--text"
             >
                     {{ $store.state.appointment.displayTime(getTime(event.date)) }}
                 </span>
             <span
-                v-if="!compact"
-                class="blue--text ml-1 mr-1"
+                    v-if="!compact"
+                    class="blue--text ml-1 mr-1"
             >
                     {{ event.client_name }}
             </span>
             <v-avatar
-                size="28px"
-                class="mr-1"
-                :title="`Исполнитель: ${event.performer && event.performer.full_name || ''}`"
+                    size="28px"
+                    class="mr-1"
+                    :title="`Исполнитель: ${event.performer && event.performer.full_name || ''}`"
             >
                 <img
                         v-if="event.performer && event.performer.avatar"
@@ -70,38 +83,42 @@
         </v-btn>
         <v-expand-x-transition>
             <v-chip
-                disabled
-                outline
-                text-color="black"
-                style="height: 40px; border: 1px solid lightgrey"
-                :style="{'background-color': `${background}!important`}"
-                v-if="display"
+                    disabled
+                    outline
+                    text-color="black"
+                    style="height: 40px; border: 1px solid lightgrey"
+                    :style="{'background-color': `${background}!important`}"
+                    v-if="display"
             >
                 <event
-                    first
-                    :event="event"
-                    @hide="display = false"
+                        first
+                        :event="event"
+                        @hide="display = false"
                 />
                 <v-icon
-                    small
-                    color="grey lighten-1"
-                    class="clickable"
-                    title="Скрыть подробности"
-                    @click="hide"
+                        small
+                        color="grey lighten-1"
+                        class="clickable"
+                        title="Скрыть подробности"
+                        @click="hide"
                 >
                     cancel
                 </v-icon>
             </v-chip>
         </v-expand-x-transition>
-        <event-context-menu
-            v-model="menu"
-            :selector="`first-${event.id}`"
-            :event="event"
-        />
     </span>
+
+        </template>
+        <event-context-menu-entry
+                :event="event"
+                @changed="visible = false"
+        />
+    </v-menu>
+
 </template>
 <script>
-    import EventContextMenu from './EventContextMenu'
+
+    import EventContextMenuEntry from './EventContextMenuEntry'
     import Event from './Event'
     export default {
         name: 'FirstEvent',
@@ -213,7 +230,7 @@
         },
         components: {
             Event,
-            EventContextMenu
+            EventContextMenuEntry
         }
     }
 </script>
