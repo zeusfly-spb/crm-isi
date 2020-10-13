1abaa0e2867bface1b36492974126f2dd23e48de
O. Saifutdinov <zeusfly@mail.ru>
2020-10-13 16:21:36 +0300

    Fixed Customer model, created Phone & SmsReports models

--- microservices/models/customer.js
+++ microservices/models/customer.js
@@ -10,7 +10,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Customer.hasMany(models.Phone, {
+        foreignKey: 'customer_id',
+        as: 'phones'
+      })
+      Customer.hasMany(models.Deal, {
+        foreignKey: 'customer_id',
+        as: 'deals'
+      })
     }
   };
   Customer.init({
