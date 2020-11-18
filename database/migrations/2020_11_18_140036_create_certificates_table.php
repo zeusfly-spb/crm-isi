6a021f3b826a0196eec2a12f42c8365d68022e0e
O. Saifutdinov <zeusfly@mail.ru>
2020-11-18 16:45:11 +0300

    Created Certificate model & migration

---
+++ database/migrations/2020_11_18_140036_create_certificates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('customer_id')->index();
+            $table->integer('nominal');
+            $table->date('start_date');
+            $table->integer('duration');
+            $table->json('history')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
